d8d2852ea2c55a0c8e72253999ae1c27881296fc
Armaan <125636741+ArmaanCh24@users.noreply.github.com>
2025-07-16 16:23:23 +0530

    Add files via upload

---
+++ passwordgen.py
@@ -0,0 +1,23 @@
+import string
+import random
+import secrets
+def genpass(length):
+    lower=string.ascii_lowercase
+    upper=string.ascii_uppercase
+    dig=string.digits
+    symb=string.punctuation
+
+    char= lower + upper + dig + symb
+    password = [
+        secrets.choice(lower),
+        secrets.choice(upper),
+        secrets.choice(dig),
+        secrets.choice(symb)
+    ]
+
+    for _ in range(length):
+        password.append(secrets.choice(char))
+    return ''.join(password)
+inp=input("Enter the lenght of the password you want")
+len=int(inp)
+print(genpass(len))
